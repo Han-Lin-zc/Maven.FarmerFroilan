e4ffd028ef6abddce77ebe37d794675b9396b91b
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-04 21:16:02 -0500

    add testArrayListAddIndex, testArrayListRemove, testArrayListSize of 5 to ChickenCoopTest

--- src/test/java/com/zipcodewilmington/froilansfarm/buildings/ChickenCoopTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/buildings/ChickenCoopTest.java
@@ -12,8 +12,11 @@ import java.util.List;
 
 public class ChickenCoopTest {
     ChickenCoop testChickenCoop = new ChickenCoop();
-    Chicken chicken = new Chicken();
     Chicken chicken1 = new Chicken();
+    Chicken chicken2 = new Chicken();
+    Chicken chicken3 = new Chicken();
+    Chicken chicken4 = new Chicken();
+    Chicken chicken5 = new Chicken();
 
     @Before
     public void setup() {
@@ -23,15 +26,15 @@ public class ChickenCoopTest {
     @Test
     public void addChickenTest() {
         List<Chicken> arrayList = new ArrayList<>();
-        Assert.assertTrue(arrayList.add(chicken));
+        Assert.assertTrue(arrayList.add(chicken1));
     }
 
     @Test
     public void testArrayListAddAll() {
         List<Chicken> arrayList = new ArrayList<>();
         List<Chicken> arrayList1 = new ArrayList<>();
-        arrayList1.add(chicken);
         arrayList1.add(chicken1);
+        arrayList1.add(chicken2);
         arrayList.addAll(arrayList1);
         Assert.assertEquals(arrayList, arrayList1);
     }
@@ -46,7 +49,28 @@ public class ChickenCoopTest {
     @Test
     public void testArrayListSize() {
         List<Chicken> arrayList = new ArrayList<>();
-        arrayList.add(chicken);
+        arrayList.add(chicken1);
+        arrayList.add(chicken2);
+        arrayList.add(chicken3);
+        arrayList.add(chicken4);
+        arrayList.add(chicken5);
+
+        Assert.assertEquals(5, arrayList.size());
+    }
+
+    @Test
+    public void testArrayListAddIndex() {
+        List<Chicken> arrayList = new ArrayList<>();
+        arrayList.add(chicken1);
+        arrayList.add(0, chicken1);
+        Assert.assertEquals(chicken1,arrayList.get(0));
+    }
+
+    @Test
+    public void testArrayListRemove() {
+        List<Chicken> arrayList = new ArrayList<>();
+        arrayList.add(chicken1);
+        Assert.assertTrue(arrayList.remove(chicken1));
     }
 }
 
